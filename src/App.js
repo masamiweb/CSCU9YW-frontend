7131b6f3dff5dce4b3ba3b4e86558b8a12b0db4e
Manjinder Singh <singhmanny71@yahoo.com>
2021-03-05 09:55:56 +0000

    installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>CSCU9YW frontend app</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
